5bae7c89e5326635bf6bb71751d08e153d73a9d5
Ragnhild Kosmo Holm <ragnhildkosmo@hotmail.com>
2013-01-15 14:43:16 +0100

    index.php
    lol

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<HTML>
+   <HEAD>
+      <TITLE>Geeks For Sale</TITLE>
+      <link rel="stylesheet" type="text/css" href="StyleIndex.css"/>
+   </HEAD>
+   <BODY>
+   </BODY>
+
+   <h1>
+	<img src="HomeButton.png"style=width:"0px;height:"0px";>
+	</h2>
+	
+   <left>  
+	<img src="SideBorder.png"style=width:"0px;height:"0px";>
+   </left>
+   	
+   <right>
+		<img src="SideBorder.png"style=width:"0px;height:"0px";>
+   </right>
+   
+   <search>
+        <input type="text" placeholder="Search here..." required>
+        <button type="submit" style="border: 0; background: transparent">
+    	<img src="SearchButton.png" width="90" height="" alt="submit" />
+</button>
+    </search> 
+
+</style>
+</HTML>

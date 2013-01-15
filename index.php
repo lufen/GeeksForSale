8af69972b3c323d308761c68fe57307791c096d5
Ragnhild Kosmo Holm <ragnhildkosmo@hotmail.com>
2013-01-15 16:39:56 +0100

    Enkel index uten bilder
    LOL

--- index.php
+++ index.php
@@ -6,26 +6,24 @@
       <link rel="stylesheet" type="text/css" href="StyleIndex.css"/>
    </HEAD>
    <BODY>
+   	   <div id="header">
+   	   	<h1><?php
+			echo "Geeks for sale!!!";
+		?></h1>
+			<s>
+		    <form class="form-wrapper cf">
+	        <input type="text" placeholder="Search here..." required>
+	        <button type="submit">Search</button>
+			</form>
+			</s>
+		</div>
+   	   
+
+	   <div id="menu">
+	   	
+   	   </div>
+   	   
+   	  
+
    </BODY>
-
-   <h1>
-	<img src="HomeButton.png"style=width:"0px;height:"0px";>
-	</h2>
-	
-   <left>  
-	<img src="SideBorder.png"style=width:"0px;height:"0px";>
-   </left>
-   	
-   <right>
-		<img src="SideBorder.png"style=width:"0px;height:"0px";>
-   </right>
-   
-   <search>
-        <input type="text" placeholder="Search here..." required>
-        <button type="submit" style="border: 0; background: transparent">
-    	<img src="SearchButton.png" width="90" height="" alt="submit" />
-</button>
-    </search> 
-
-</style>
 </HTML>

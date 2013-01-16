251fefe02513c55eabacc1261bac7d44f545bcb1
Ragnhild Kosmo Holm <ragnhildkosmo@hotmail.com>
2013-01-16 11:44:42 +0100

    Fixed top menu

---
+++ shoppingbasket.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<HTML>
+   <HEAD>
+      <TITLE>Geeks For Sale</TITLE>
+      <link rel="stylesheet" type="text/css" href="StyleIndex.css"/>
+   </HEAD>
+   <BODY>
+   	   <div id="header">
+            <?php include("topmenu.php"); ?>
+   	   	<h1>
+               <?php
+   			      echo "Geeks for sale!!!";
+   		       ?>
+            </h1>
+			<s>
+      	    <form class="form-wrapper cf">
+                 <input type="text" placeholder="Search here..." required>
+                 <button type="submit">Search</button>
+      		</form>
+			</s>
+		</div>
+   	   
+	   <div id="menu">
+         <?php
+         require_once 'menu.php';
+         ?>
+	   </div>
+
+      <div id="content">
+         <?php
+            echo "Shopping Basket";
+         ?>
+      </div>
+   </BODY>
+</HTML>
--- topmenu.php
+++ topmenu.php
@@ -2,5 +2,5 @@
 	<a href="login.php">Log in</a><br>
 	<a href="mypage.php">My Page</a><br>
 	<a href="register.php">Register</a><br>
-	<a href="register.php">Shopping Basket</a><br>
+	<a href="shoppingbasket.php">Shopping Basket</a><br>
 </topMenu>

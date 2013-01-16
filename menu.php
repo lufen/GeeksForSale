3e35c46d300f6923bf3a0b077c7279ed96c90ece
Christopher Haugen <chrihau@gmail.com>
2013-01-16 12:45:21 +0100

    Fixed error in menu

--- menu.php
+++ menu.php
@@ -10,10 +10,10 @@ require_once "db.php";
  	echo $row['categoryName'];
 	// Get subcategories
 	$sql = "Select * from subcategory where categoryid = ".$row['id'];
-	$sth = $db->prepare($sql);
-	$sth->execute();
-  	$sth->setFetchMode(PDO::FETCH_ASSOC);
-	while($rows = $sth->fetch()){
+	$sth2 = $db->prepare($sql);
+	$sth2->execute();
+  	$sth2->setFetchMode(PDO::FETCH_ASSOC);
+	while($rows = $sth2->fetch()){
 		echo "<br>";
 	    echo "<a href=\"productcategory.php?id=".$rows['id']."\">".$rows['name']."</a><br>";
 	}

bb6a64a78ce99e73b690dfa3a5aa8e4cb9d204cb
Christopher Haugen <chrihau@gmail.com>
2013-01-16 09:55:35 +0100

    Top menu added

---
+++ register.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
+<HTML>
+   <HEAD>
+      <TITLE>Geeks For Sale</TITLE>
+      <link rel="stylesheet" type="text/css" href="StyleIndex.css"/>
+   </HEAD>
+   <BODY>
+   	   <div id="header">
+            <a href="login.php">Log in</a><br>
+            <a href="mypage.php">My Page</a><br>
+   	   	<h1>
+               <?php
+   			      echo "Geeks for sale!!!";
+   		       ?>
+            </h1>
+			<s>
+      	    <form class="form-wrapper cf">
+                 <input type="text" placeholder="Search here..." required>
+                 <button type="submit">Search</button>
+      		</form>
+			</s>
+		</div>
+   	   
+	   <div id="menu">
+         <?php
+         require_once 'menu.php';
+         ?>
+	   </div>
+   </BODY>
+</HTML>

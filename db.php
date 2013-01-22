b3a1b37c65249a44f66b256fe3e00b16f801ba5b
lufen <chrihau@gmail.com>
2013-01-22 16:23:12 +0100

    Update db.php
    Changed DB user

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Make a php data object
 try {
-	$db = new PDO('mysql:host=127.0.0.1;dbname=geeksforsale', 'root', '');
+	$db = new PDO('mysql:host=127.0.0.1;dbname=geeksforsale', 'qfs', '');
 } catch (PDOException $e) {
     die ('Could not connect to server : ' . $e->getMessage());
 }

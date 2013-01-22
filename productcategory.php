195cf547130dc3aa32d36f271f796b4a2a12a4ce
Christopher Haugen <chrihau@gmail.com>
2013-01-22 12:44:21 +0100

    Added possebility to remove object from shoppingbasket

--- productcategory.php
+++ productcategory.php
@@ -29,18 +29,24 @@
       <div id="content">
          <?php
             $id = $_GET['id'];
-            echo "Category ID: " . $id."<br>";
-            echo "Products in this category: ";
-
-            $sql = "Select id,name,price from products where categoriID= ".$id;
+            $sql = "Select * from subcategory where id=:id";
             $sth = $db->prepare($sql);
+            $sth->bindValue (':id', $id);
             $sth->execute();
             $sth->setFetchMode(PDO::FETCH_ASSOC);  
-            $array = array();
+            $row = $sth->fetch();
+            echo $row['name']."<br>";
+            echo "Products in this category: ";
+
+            $sql = "Select * from products where categoriID=:id";
+            $sth = $db->prepare($sql);
+             $sth->bindValue (':id', $id);
+            $sth->execute();
+            $sth->setFetchMode(PDO::FETCH_ASSOC);  
             echo "<br>";
             while($row = $sth->fetch()){
-               echo "<a href=\"productdetails.php?id=".$row['id']."\">".$row['name']."</a> ";
-               echo $row['price']." <br>";
+               if($row['forSale'] != "0")
+                  echo "<a href=\"productdetails.php?id=".$row['id']."\">".$row['name']."</a> Price: ".$row['price']." In Stock: ".$row['onStock']."<br>";
             }
          ?>
       </div>

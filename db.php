9aac37e2176f6a3ec63b45204a4002cc83391fcf
lufen <chrihau@gmail.com>
2013-01-22 16:25:03 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Make a php data object
 try {
-	$db = new PDO('mysql:host=127.0.0.1;dbname=geeksforsale', 'qfs', '');
+	$db = new PDO('mysql:host=127.0.0.1;dbname=geeksforsale', 'gfs', '');
 } catch (PDOException $e) {
     die ('Could not connect to server : ' . $e->getMessage());
 }

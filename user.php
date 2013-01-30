9bf2e278916d3259c00bd3b61309e430f490f17c
Christopher Haugen <chrihau@gmail.com>
2013-01-30 09:47:34 +0100

    Moved hashing of password from db.php to user.php

--- user.php
+++ user.php
@@ -21,6 +21,13 @@
 			 }
 	}
 
+function convertPlainTextToEncrypted($password,$uid){
+	// Convert a password from plaintext into a encrypted one
+	$salt = "c63b03f38470b6c30abdb8d2b7e59b14ddeb6a0d6e56956b0df44a0a8dd3cf6980dcbd907cb9aa1ea9edccb37739e20e240ddeafd68d386d289cd68ee9343167";
+	$hash = sha1($salt.$uid.$password);
+	return $hash;
+}
+
 function registerUser($db,$name, $streetAdress,$postCode,$Country, $email, $password){
 	require_once 'sessionStart.php';
 	$db->beginTransaction();

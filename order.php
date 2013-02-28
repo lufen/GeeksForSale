eb75b75613d7a67405a700d62d2a651d81bebf78
Christopher Haugen <chrihau@gmail.com>
2013-02-28 12:50:22 +0100

    Fixing indented

--- order.php
+++ order.php
@@ -1,77 +1,78 @@
 <?php
 require_once 'sessionStart.php';
 require_once 'db.php';
+require_once 'user.php';
 // Check if this is an update of the shoppingbasket, and modify the amounts.
- if(isset($_POST['placed'])){
- 	 $sql = 'INSERT INTO orders (userID)VALUES (:userID)';
-  	$sth = $db->prepare ($sql);
-  	$sth->bindValue (':userID', $_SESSION['id']);
-  	$sth->execute ();
+if(isset($_POST['placed'])){
+ $sql = 'INSERT INTO orders (userID)VALUES (:userID)';
+ $sth = $db->prepare ($sql);
+ $sth->bindValue (':userID', $_SESSION['id']);
+ $sth->execute ();
   	// Get ID of last insert
-  	$orderID = $db->lastInsertId();
-  	$ordersplaced = 0;
-  	$shouldHaveBeenDone = 0;
- 	 foreach ($_SESSION as $key => $quantity){
-    if($key ==="id")
-      continue;
- 	 	$shouldHaveBeenDone++;
+ $orderID = $db->lastInsertId();
+ $ordersplaced = 0;
+ $shouldHaveBeenDone = 0;
+ foreach ($_SESSION as $key => $quantity){
+  if($key ==="id")
+    continue;
+  $shouldHaveBeenDone++;
 		// Get price from DB
-		$sqltmp = 'Select * from products where id = :id';
-		$sthTmp = $db->prepare ($sqltmp);
-		$sthTmp->bindValue(':id',substr($key,1));
-		$sthTmp->execute ();
-		$sthTmp->setFetchMode(PDO::FETCH_ASSOC);  
-		$row = $sthTmp->fetch();
-		$price = $row['price'];
-		
-	 	$sql = 'INSERT INTO orderdetail (orderID, productID, price, qty, sendt)VALUES (:orderID, :productID, :price, :qty, :sendt)';
-	  	$sth = $db->prepare ($sql);
-	  	$sth->bindValue (':orderID', $orderID);
-	  	$sth->bindValue (':productID', substr($key,1));
-	  	$sth->bindValue (':price', $price);
-	  	$sth->bindValue (':qty', $quantity);
-	  	$sth->bindValue (':sendt', "0");
-	  	$ordersplaced += $sth->execute ();
- 	 }
- 	 if($ordersplaced === $shouldHaveBeenDone && $shouldHaveBeenDone != 0){
- 	 	echo "Thanks for the order. It will be shipped soon";
-    emptyBasket();
- 	 }else{
- 	 	echo $ordersplaced;
- 	 	echo $shouldHaveBeenDone;
- 	 }
-  }
-  ?>
+  $sqltmp = 'Select * from products where id = :id';
+  $sthTmp = $db->prepare ($sqltmp);
+  $sthTmp->bindValue(':id',substr($key,1));
+  $sthTmp->execute ();
+  $sthTmp->setFetchMode(PDO::FETCH_ASSOC);  
+  $row = $sthTmp->fetch();
+  $price = $row['price'];
+
+  $sql = 'INSERT INTO orderdetail (orderID, productID, price, qty, sendt)VALUES (:orderID, :productID, :price, :qty, :sendt)';
+  $sth = $db->prepare ($sql);
+  $sth->bindValue (':orderID', $orderID);
+  $sth->bindValue (':productID', substr($key,1));
+  $sth->bindValue (':price', $price);
+  $sth->bindValue (':qty', $quantity);
+  $sth->bindValue (':sendt', "0");
+  $ordersplaced += $sth->execute ();
+}
+if($ordersplaced === $shouldHaveBeenDone && $shouldHaveBeenDone != 0){
+  echo "Thanks for the order. It will be shipped soon";
+  emptyBasket();
+}else{
+  echo $ordersplaced;
+  echo $shouldHaveBeenDone;
+}
+}
+?>
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
-   "http://www.w3.org/TR/html4/strict.dtd">
+ "http://www.w3.org/TR/html4/strict.dtd">
 <HTML>
-   <HEAD>
-      <TITLE>Geeks For Sale</TITLE>
-      <link rel="stylesheet" type="text/css" href="StyleIndex.css"/>
-   </HEAD>
-   <BODY>
-   	   <div id="header">
-            <?php include("topmenu.php"); ?>
-   	   	<h1>
-               <?php
-   			      echo "Geeks for sale!!!";
-   		       ?>
-            </h1>
-			<s>
-      	    <form class="form-wrapper cf">
-                 <input type="text" placeholder="Search here..." required>
-                 <button type="submit">Search</button>
-      		</form>
-			</s>
-		</div>
-   	   
-	   <div id="menu">
-         <?php
-         require_once 'menu.php';
-         ?>
-	   </div>
-      <div id="content">
-	   </div>
-   </BODY>
+ <HEAD>
+  <TITLE>Geeks For Sale</TITLE>
+  <link rel="stylesheet" type="text/css" href="StyleIndex.css"/>
+</HEAD>
+<BODY>
+ <div id="header">
+  <?php include("topmenu.php"); ?>
+  <h1>
+   <?php
+   echo "Geeks for sale!!!";
+   ?>
+ </h1>
+ <s>
+   <form class="form-wrapper cf">
+     <input type="text" placeholder="Search here..." required>
+     <button type="submit">Search</button>
+   </form>
+ </s>
+</div>
+
+<div id="menu">
+ <?php
+ require_once 'menu.php';
+ ?>
+</div>
+<div id="content">
+</div>
+</BODY>
 </HTML>

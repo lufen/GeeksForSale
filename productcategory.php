7d710be7f6543262712295f5738e9061d0a9a1a6
Christopher Haugen <chrihau@gmail.com>
2013-03-05 14:01:19 +0100

    Fixing some nice design

--- productcategory.php
+++ productcategory.php
@@ -26,13 +26,12 @@
    $row = $sth->fetch();
    echo "<t0>".$row['name']."</t0><br>";
 
-   $sql = "Select * from products where categoriID=:id";
+   $sql = "Select * from products where categoriID=:id AND forSale=1";
    $sth = $db->prepare($sql);
    $sth->bindValue (':id', $id);
    $sth->execute();
    $sth->setFetchMode(PDO::FETCH_ASSOC);  
    while($row = $sth->fetch()){
-      if($row['forSale'] != "0"){
           echo "<div id=product>";
           echo "<t1><a href=\"productdetails.php?id=".$row['id']."\">".$row['name']."</a></t1></br>";
           echo "<t3>In Stock: ".$row['onStock']."</t3></br>";
@@ -40,11 +39,8 @@
             echo "<t2>Price: $".intval($row['price'])*(intval($row['rabatt'])/100)."</t2>";
          }else{
             echo "<t2>Price: $".$row['price']."</t2>";
-          
         }
-          
           echo "</div>";
-       }
    }
    ?>
 </div>
--- search.php
+++ search.php
@@ -16,16 +16,25 @@
    ?>
 </div>
 <div id="content">
+  <t0>Search results </t0>
    <?php
    $search = '\'%'.mysql_real_escape_string($_POST['search']).'%\'';
-   $sql = "SELECT * from products where name like $search OR id=:search or info like $search";
+   $sql = "SELECT * from products where name like $search OR id=:search or info like $search AND forSale=1";
    $sth = $db->prepare($sql);
    $sth->bindValue (':search', $_POST['search']);
    $sth->execute();
    $sth->setFetchMode(PDO::FETCH_ASSOC);  
    while($row = $sth->fetch()){
-      if($row['forSale'] != "0")
-         echo "<a href=\"productdetails.php?id=".$row['id']."\">".$row['name']."</a> Price: ".$row['price']." In Stock: ".$row['onStock']."<br>";
+          echo "<div id=product>";
+          echo "<t1><a href=\"productdetails.php?id=".$row['id']."\">".$row['name']."</a></t1></br>";
+          echo "<t3>In Stock: ".$row['onStock']."</t3></br>";
+          if(intval($row['rabatt'])!= 0){
+            echo "<t2>Price: $".intval($row['price'])*(intval($row['rabatt'])/100)."</t2>";
+         }else{
+            echo "<t2>Price: $".$row['price']."</t2>";
+          
+        }
+          echo "</div>";
    }
    ?>
 </div>

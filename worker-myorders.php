4b2a1a859a2fc0403cec193137e159ea276b4053
Christopher Haugen <chrihau@gmail.com>
2013-03-05 18:41:24 +0100

    Fixing buttons

--- worker-myorders.php
+++ worker-myorders.php
@@ -81,6 +81,8 @@ include 'Geeksforsaletop.php';
 ?>
 
 <div id="content">
+  <button> <a href="worker-myorders.php"> My orders </a> </button>
+  <button> <a href="worker.php"> Orders not taken </a> </button>
  <?php
  echo "<p>My orders not shipped";
  echo "</br>";

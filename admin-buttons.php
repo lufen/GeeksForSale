af8a670c966f53f923eec0c8e2bb6dc4e660165f
Ragnhild Kosmo Holm <ragnhildkosmo@hotmail.com>
2013-03-05 17:46:31 +0100

    werkltgjthjkl

---
+++ admin-buttons.php
@@ -0,0 +1,4 @@
+  <button> <a href="admin-addUser.php"> Add user        </a> </button>
+  <button> <a href="admin-addProducts.php"> Add product        </a> </button>
+  <button> <a href="admin-addCategories.php"> Add Category        </a> </button>
+  <button> <a href="admin-blackListUsers.php"> Blacklist a user        </a> </button></br>

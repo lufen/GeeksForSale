8f0c8928b032920b0050335d9ec0b8002ea12c88
Christopher Haugen <chrihau@gmail.com>
2013-03-05 17:04:03 +0100

    Adding files

---
+++ admin-blackListUsers.php
@@ -0,0 +1,22 @@
+<?php
+require_once "user.php";
+CheckIfAdminLoggedIn();
+?>
+
+<?php
+include 'Geeksforsaletop.php';
+?>
+<div id="content">
+ <?php
+ if(isset($_POST['placed'])){
+  try { 
+    OrderPlaced();
+    echo "<p> Thanks for your order";
+  } catch (Exception $e){
+    echo $e->getMessage();
+  }
+}
+?>
+</div>
+</BODY>
+</HTML>

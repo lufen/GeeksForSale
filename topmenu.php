19dbe309aa3634a2c6d8cf1967ac20bc1a11a748
Christopher Haugen <chrihau@gmail.com>
2013-03-05 13:53:36 +0100

    Fixing ugly stuff

--- topmenu.php
+++ topmenu.php
@@ -4,9 +4,10 @@
 	require_once 'sessionStart.php';
 	if(isset($_SESSION['id'])){
 		echo "<a href=\"index.php?logout=1\">Log Out</a><br>";
-		if($_SESSION['userLevel'] == 0){
+		if(isset($_SESSION['id'])){
 			echo "<a href=\"mypage.php\">My Page</a><br>";
-		}else if($_SESSION['userLevel'] == 1){
+		}
+		if($_SESSION['userLevel'] == 1){
 			echo "<a href=\"worker.php\">My Worker Page</a><br>";
 		}else if($_SESSION['userLevel'] == 2){
 			echo "<a href=\"admin.php\">My Admin Page</a><br>";

7cbdda678b2e1d6e0a22c6fd9c00b3d51dff33f6
Christopher Haugen <chrihau@gmail.com>
2013-03-05 20:03:48 +0100

    fixing

--- worker.php
+++ worker.php
@@ -22,8 +22,7 @@ include 'Geeksforsaletop.php';
 ?>
 
 <div id="content">
-  <button> <a href="worker-myorders.php"> My orders </a> </button>
-  <button> <a href="worker.php"> Orders not taken </a> </button>
+<?php include 'worker-buttons.php'; ?>
    <?php
    echo "<p>Orders not shipped or taken yet";
    OrderNotYetShipped();
